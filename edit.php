ba657db0c4088038cf52789b4d649828bc44a600
Elango Surya <94896279+Elangosurya1998@users.noreply.github.com>
2024-11-28 18:01:14 +0530

    Update edit.php

--- edit.php
+++ edit.php
@@ -10,19 +10,41 @@ try {
 
   $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-  $sql = "UPDATE myguests SET firstname='john',lastname='Doe' WHERE id=$id";
+}catch(PDOException $e){
+  echo 'Error :'. $e->getMessage();
+}
 
-  // Prepare statement
-  $stmt = $conn->prepare($sql);
+?>
+<html>
+<head>
+	<title>Edit Data</title>
+</head>
+<body>
+	<h1>Simple Edit Form</h1>
+	<br/><br/>
 
-  // execute the query
-  $stmt->execute();
+	<form action="update.php" method="post" name="form1">
+    <input type="hidden" name="id" value="<?php echo $result['id']; ?>">
+		<table width="25%" border="0">
+			<tr> 
+				<td>First Name</td>
+				<td><input type="text" name="firstname" value="<?php echo $result['firstname']; ?>" required></td>
+			</tr>
+			<tr> 
+				<td>Last Name</td>
+				<td><input type="text" name="lastname" value="<?php echo $result['lastname']; ?>" required></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="email" name="email" value="<?php echo $result['email']; ?>" required></td>
+			</tr>
+			<tr> 
+				<td></td>
+				<td><input type="submit" name="Submit" value="Update"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
 
-  // echo a message to say the UPDATE succeeded
-  echo $stmt->rowCount() . " records UPDATED successfully";
-
-    // echo "New record created successfully. Last inserted ID is: " . $last_id;
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
-} 
  
