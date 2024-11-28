f9f59e0d4022e7b3ee21c0ff787502d2f8ff9b85
Elango Surya <94896279+Elangosurya1998@users.noreply.github.com>
2024-11-28 18:01:56 +0530

    Update update.php

--- update.php
+++ update.php
@@ -1,3 +1,27 @@
 <?php
 
 include_once('dbconnection.php');
+
+$id = $_POST['id'];
+
+$firstName = $_POST['firstname'];
+$lastName =$_POST['lastname'];
+$email = $_POST['email'];
+
+try{
+
+    $sql = "UPDATE MyGuests SET firstname=:firstname,lastname=:lastname,email=:email WHERE id=$id";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bindParam(':firstname',$firstName);
+    $stmt->bindParam(':lastname',$lastName);
+    $stmt->bindParam(':email',$email);
+    
+    $stmt->execute();
+
+    header('location: index.php');
+
+} catch(PDOException $e) {
+    echo 'Error:'. $e->getMessage();
+} 
