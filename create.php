58feed7db5e0129157c95fcdd330832c634460e3
Elango Surya <94896279+Elangosurya1998@users.noreply.github.com>
2024-11-29 18:48:03 +0530

    Update create.php

--- create.php
+++ create.php
@@ -2,31 +2,36 @@
 <html>
 <head>
 	<title>Add Data</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-	<h1>Simple Form</h1>
+	<h1 class="alert alert-warning">Simple Form</h1>
 	<br/><br/>
-
+	<div class="container mt-3">
 	<form action="store.php" method="post" name="form1">
-		<table width="25%" border="0">
-			<tr> 
-				<td>First Name</td>
-				<td><input type="text" name="firstname" required></td>
-			</tr>
-			<tr> 
-				<td>Last Name</td>
-				<td><input type="text" name="lastname" required></td>
-			</tr>
-			<tr> 
-				<td>Email</td>
-				<td><input type="email" name="email" required></td>
-			</tr>
-			<tr> 
-				<td></td>
-				<td><input type="submit" name="Submit" value="Add"></td>
-			</tr>
-		</table>
-	</form>
+    <div class="mb-3 mt-3">
+      <label for="email">First Name:</label>
+	  <input class="form-control" type="text" name="firstname"  placeholder="Enter Your Name" required>
+    </div>
+    <div class="mb-3">
+      <label for="pwd">Last Name:</label>
+	  <input class="form-control" type="text" name="lastname" required>
+    </div>
+	<div class="mb-3">
+      <label for="pwd">Email:</label>
+      <input class="form-control" type="email" name="email" required>
+    </div>
+    <div class="form-check mb-3">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <input class="btn btn-success" type="submit" name="Submit" value="Add">
+  </form>
+</div>
 </body>
 </html>
